44a689e0c989ee5333391f11c0b07d3164ef9c03
Lucas Romero Di Benedetto <lucasromerodb@gmail.com>
2018-11-13 23:08:41 -0300

    styled components for page and header

--- components/Page.js
+++ components/Page.js
@@ -1,15 +1,40 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import Meta from "./Meta";
 import Header from "./Header";
+
+const theme = {
+  red: "#ff0000",
+  black: "#393939",
+  grey: "#3a3a3a",
+  lightgrey: "#e1e1e1",
+  offWhite: "#ededed",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  background: ${props => props.theme.red};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends Component {
   render() {
     return (
-      <Fragment>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </Fragment>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
